00ee649f11b25ab5cac66cf561de579a5c2954d0
Jesper Gulmann Henriksen <jgh@wincubate.net>
2019-12-10 10:52:27 +0100

    Update for W50 course

--- Workshop/C.2/Solution/Admin/Admin.UI.WpfApp/DependencyInjectionConfig.cs
+++ Workshop/C.2/Solution/Admin/Admin.UI.WpfApp/DependencyInjectionConfig.cs
@@ -24,7 +24,7 @@ namespace Admin.UI.WpfApp
                 .RegisterType<ICreateUserService, CreateUserService>()
                 .RegisterType<IMessageTemplateRepository, SqlMessageTemplateRepository>()
                 .RegisterType<MessageTemplateContext>()
-                .RegisterType<SendGridEmailTransmissionStrategy>()
+                .RegisterType<Messenger>()
                 .RegisterType<IMessageTransmissionStrategy,RedirectionMessageTransmissionStrategyProxy>(
                     new InjectionConstructor(
                         new ResolvedParameter<SendGridEmailTransmissionStrategy>(),
